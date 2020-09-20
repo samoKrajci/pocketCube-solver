f9ef0fcca7aa339c0c382ac933f47ef8d4fad17c
samoKrajci <samo.krajci@gmail.com>
2020-09-20 21:03:31 +0200

    add the whole thing..

--- cube.hs
+++ cube.hs
@@ -1,6 +1,7 @@
 import qualified Data.Map as Map
 import qualified Data.Sequence as Sequence
 
+-- queue
 qPush :: a -> Sequence.Seq a -> Sequence.Seq a
 qPush a s = s Sequence.:|> a
 
@@ -19,6 +20,16 @@ data Side = Side Char Char Char Char
 
 -- order: front right up back left down
 data Cube = Cube Side Side Side Side Side Side
+    deriving (Eq, Show, Ord)
+
+-- cube map in format:
+-- UU
+-- UU
+-- FFRRBBLL
+-- FFRRBBLL
+-- DD
+-- DD
+data CubeMap = CubeMap String String String String String String
     deriving (Eq, Show, Ord)
 
 rotF :: Cube -> Cube
@@ -78,6 +89,17 @@ rotU (Cube
 rotUr :: Cube -> Cube
 rotUr c = rotU $ rotU $ rotU c
 
+-- parseInput
+cubeMapToCube :: CubeMap -> Cube
+cubeMapToCube (CubeMap r0 r1 r2 r3 r4 r5) =
+    Cube
+    (Side (r2!!0) (r2!!1) (r3!!1) (r3!!0))
+    (Side (r2!!2) (r2!!3) (r3!!3) (r3!!2))
+    (Side (r0!!0) (r0!!1) (r1!!1) (r1!!0))
+    (Side (r2!!4) (r2!!5) (r3!!5) (r3!!4))
+    (Side (r2!!6) (r2!!7) (r3!!7) (r3!!6))
+    (Side (r4!!0) (r4!!1) (r5!!1) (r5!!0))
+
 solvedCube :: Cube
 solvedCube = Cube (Side 'g' 'g' 'g' 'g') (Side 'r' 'r' 'r' 'r') (Side 'w' 'w' 'w' 'w') (Side 'b' 'b' 'b' 'b') (Side 'o' 'o' 'o' 'o') (Side 'y' 'y' 'y' 'y')
 -- solvedCube = Cube (Side 'o' 'o' 'o' 'o') (Side 'b' 'b' 'b' 'b') (Side 'y' 'y' 'y' 'y') (Side 'r' 'r' 'r' 'r') (Side 'g' 'g' 'g' 'g') (Side 'w' 'w' 'w' 'w')
@@ -97,14 +119,13 @@ isSolved (Cube a b c d e f)
 
 
 allRotations :: [Cube -> Cube]
-allRotations = [rotU, rotUr, rotF, rotFr, rotR, rotRr]
+allRotations = [rotUr, rotU, rotFr, rotF, rotRr, rotR]
 
 allRotationsString :: [String]
-allRotationsString = ["U", "U'", "F", "F'", "R", "R'"]
+allRotationsString = ["U'", "U", "F'", "F", "R'", "R"]
 
 allRotationsTuple :: [(Cube -> Cube, String)]
 allRotationsTuple = zip allRotations allRotationsString
-
 
 bfs :: Sequence.Seq (Cube, [String]) -> Map.Map Cube [String] -> Map.Map Cube [String]
 bfs queue m 
@@ -118,18 +139,30 @@ bfs queue m
             else let nQueue = foldr (\(rot, rotStr) q -> qPush ((rot cube), ([rotStr] ++ str)) q) (qPop queue) allRotationsTuple in
                 bfs nQueue (Map.insert cube str m)
 
-solve :: Cube -> [String]
+solve :: Cube -> String
 solve cube 
     | res == Nothing = error "invalid cube"
-    | otherwise = let Just s = res in reverse s
+    | otherwise = let Just s = res in concatWithSpaces $ addDoubleTurns $ reverse s
     where res = Map.lookup xCube $ bfs (Sequence.singleton (cube, [])) Map.empty
 
+addDoubleTurns :: [String] -> [String]
+addDoubleTurns ar = init $ foldr (\x (y:ys) -> if x == y then ["2" ++ (take 1 y)] ++ ys else [x] ++ [y] ++ ys) [""] ar
 
--- (Cube (Side 'w' 'y' 'g' 'g') (Side 'b' 'r' 'b' 'w') (Side 'g' 'y' 'r' 'r') (Side 'g' 'w' 'b' 'w') (Side 'o' 'b' 'o' 'o') (Side 'y' 'r' 'o' 'y'))
--- (Cube (Side 'y' 'g' 'w' 'y') (Side 'r' 'b' 'o' 'b') (Side 'y' 'w' 'w' 'b') (Side 'o' 'r' 'b' 'w') (Side 'g' 'r' 'g' 'o') (Side 'o' 'r' 'g' 'y'))
--- (Cube (Side 'b' 'r' 'b' 'g') (Side 'g' 'o' 'r' 'r') (Side 'g' 'b' 'y' 'r') (Side 'w' 'r' 'b' 'o') (Side 'w' 'i' 'w' 'o') (Side 'o' 'w' 'g' 'i'))
--- (Cube (Side 'y' 'r' 'y' 'g') (Side 'y' 'r' 'r' 'o') (Side 'r' 'y' 'b' 'o') (Side 'g' 'b' 'w' 'g') (Side 'w' 'b' 'w' 'o') (Side 'o' 'g' 'w' 'b'))
--- (Cube (Side 'b' 'r' 'g' 'w') (Side 'y' 'r' 'g' 'y') (Side 'g' 'y' 'b' 'w') (Side 'g' 'r' 'b' 'o') (Side 'w' 'r' 'b' 'o') (Side 'o' 'o' 'w' 'y'))
+concatWithSpaces :: [String] -> String
+concatWithSpaces ar = foldr (\a b -> a ++ " " ++ b) "" ar
 
-main :: IO()
-main = print $ solve (Cube (Side 'b' 'r' 'g' 'w') (Side 'y' 'r' 'g' 'y') (Side 'g' 'y' 'b' 'w') (Side 'g' 'r' 'b' 'o') (Side 'w' 'r' 'b' 'o') (Side 'o' 'o' 'w' 'y'))
+outputWrapper :: String -> String
+outputWrapper "" = "Cube is already solved."
+outputWrapper str = str
+
+main = do
+    putStrLn "Enter cube map:"  
+    r0 <- getLine  
+    r1 <- getLine  
+    r2 <- getLine  
+    r3 <- getLine  
+    r4 <- getLine  
+    r5 <- getLine  
+    putStrLn "Calculating..."  
+    let cube = cubeMapToCube (CubeMap r0 r1 r2 r3 r4 r5) in
+        putStrLn $ outputWrapper $ solve cube
